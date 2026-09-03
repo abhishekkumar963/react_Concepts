9b1cae178f71aef629b51fdad1ed81616e8604c3
Abhishek Kumar <abhishekkumarsingh845454@gmail.com>
2026-09-03 21:11:37 +0530

    Components

---
+++ Components/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h2>Welcome to my website</h2>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
